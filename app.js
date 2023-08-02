029788df1940855c6c2502b7d1565de7b694e7b8
EspeditoJunior <e.jr42@hotmail.com>
2023-08-01 22:04:34 -0300

    yarn

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
-const validator = require('validator')
 const chalk = require('chalk')
-const notes = require('./notes.js')
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
 
-console.log(validator.isEmail('e.jr42@hotmail.com'));
-console.log(validator.isURL('e.jr42@hotmail.com'));
-console.log(notes())
-console.log(chalk.green(
-	'I am a green line ' +
-	chalk.blue.underline.bold('with a blue substring') +
-	' that becomes green again!'
-))
+yargs.version('1.1.0')
+yargs.command({
+	command: 'add',
+	describe: 'Add a new note',
+	builder: {
+		title: {
+			describe: 'Note title',
+			demandOption: true,
+			type: 'string'
+		},
+		body: {
+			describe: 'Note body',
+			demandOption: true,
+			type: 'string'
+		}
+	},
+	handler: function(argv){
+		console.log('Title: ' + argv.title + ', Body: ' + argv.body)
+	}
+})
+
+yargs.command({
+	command: 'remove',
+	describe: 'Remove a note',
+	handler: function(){
+		console.log('Removing a note')
+	}
+})
+
+yargs.command({
+	command: 'list',
+	describe: 'List a note',
+	handler: function(){
+		console.log('Listing all notes')
+	}
+})
+
+yargs.command({
+	command: 'read',
+	describe: 'Read a note',
+	handler: function(){
+		console.log('Reading a note')
+	}
+})
+
+yargs.parse()
